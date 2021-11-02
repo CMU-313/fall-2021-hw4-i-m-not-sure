f73d772b1719b8540e96379b6c2d6c9a3f7ca2b5
Huahan98 <huahanqu@MacBook-Pro.local>
2021-11-02 17:16:17 -0400

    add more tests

--- dockerfile/test_app.py
+++ dockerfile/test_app.py
@@ -10,9 +10,6 @@ from flask import g
 from urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 
-
-PREDICT_URL = 'http://localhost:5000/'
-
 def init_db(app):
     print(app)
     if "db" not in g:
@@ -23,6 +20,8 @@ def init_db(app):
 
     return g.db
 
+BASE_URL = 'http://localhost:5000/'
+PREDICT_URL = 'http://localhost:5000/predict?'
 @pytest.fixture
 def client():
     http_session = requests.Session()
@@ -30,15 +29,39 @@ def client():
     http_session.mount('http://', HTTPAdapter(max_retries=retries))
     return http_session
 
-def test_empty_request(client):
-    response = client.get(PREDICT_URL)
-    print("here" + response.text)
+def test_flask_is_up(client):
+    response = client.get(BASE_URL)
     assert response.status_code == 200
+    assert response.text == "try the predict route it is great!"
 
+def test_predict_empty_request(client):
+    response = client.get(PREDICT_URL)
+    assert response.status_code == 500
+
+def test_predict_insufficent_args(client):
+    response = client.get(PREDICT_URL + "age=18&&health=3&&Medu=1")
+    assert response.status_code == 500
+
+def test_predict_model_too_many_args(client):
+    response = client.get(PREDICT_URL + "age=18&&absences=6&&health=3&&Medu=4&&Fedu=4&&studytime=2&&traveltime=2&&random1=a&&random2=b&&random3=4")
+    assert response.status_code == 200
+    assert response.text == '0\n'
+
+def test_predict_model_correct_output(client):
+    response = client.get(PREDICT_URL + "age=18&&absences=6&&health=3&&Medu=4&&Fedu=4&&studytime=2&&traveltime=2")
+    assert response.status_code == 200
+    assert response.text == '0\n'
+
+def test_flask_and_model_consistency(client):
+    for i in range(10):
+        response = client.get(PREDICT_URL + "age=18&&absences=6&&health=3&&Medu=4&&Fedu=4&&studytime=2&&traveltime=2")
+        assert response.status_code == 200
+        assert response.text == '0\n'
 
 if __name__ == "__main__":
     http_session = requests.Session()
     retries = Retry(total=10, backoff_factor=0.1)
     http_session.mount('http://', HTTPAdapter(max_retries=retries))
     response = http_session.get(PREDICT_URL)
+    print(response.status_code)
     print(response.text)
